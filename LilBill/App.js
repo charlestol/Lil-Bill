488183d5492e71cde4296250a4e23c6efb01e341
miguelhx <mhernandez401405@gmail.com>
2018-11-07 13:57:56 -0800

    update code, update readme

--- LilBill/App.js
+++ LilBill/App.js
@@ -6,7 +6,7 @@ import {
   View
 } from 'react-native';
 
-const BEST_MATCH_THRESHOLD = 0.5;
+const BEST_MATCH_THRESHOLD = 0.8;
 
 import CoreMLImage from "react-native-core-ml-image";
 
@@ -15,7 +15,8 @@ export default class App extends Component<{}> {
   constructor(props) {
     super(props);
     this.state = {
-      bestMatch: null
+      bestMatch: null,
+      classifications: []
     };
   }
 
@@ -24,6 +25,7 @@ export default class App extends Component<{}> {
 
     if (classifications && classifications.length > 0) {
       // Loop through all of the classifications and find the best match
+      // this.setState({ classifications });
       classifications.forEach((classification) => {
         if (!bestMatch || classification.confidence > bestMatch.confidence) {
           bestMatch = classification;
@@ -32,19 +34,13 @@ export default class App extends Component<{}> {
 
       // Is best match confidence better than our threshold?
       if (bestMatch.confidence >= BEST_MATCH_THRESHOLD) {
-        this.setState({
-          bestMatch: bestMatch
-        });
+        this.setState({ bestMatch: bestMatch });
       } else {
-        this.setState({
-          bestMatch: null
-        });
+        this.setState({ bestMatch: null });
       }
 
     } else {
-      this.setState({
-        bestMatch: null
-      });
+      this.setState({ bestMatch: null });
     }
 
   }
@@ -52,21 +48,39 @@ export default class App extends Component<{}> {
 
   render() {
     let classification = null;
+    let { classifications } = this.state || [];
 
     if (this.state.bestMatch) {
-      if (this.state.bestMatch && this.state.bestMatch.identifier && this.state.bestMatch.identifier === "hotdog") {
-        classification = "🌭 Hot Dog 🌭";
-      } else {
-        classification = "Not hot dog";
+      if (this.state.bestMatch && this.state.bestMatch.identifier && this.state.bestMatch.identifier === "ten-dollar-bill") {
+        classification = "TEN DOLLAR BILL";
+      }
+      else if (this.state.bestMatch && this.state.bestMatch.identifier && this.state.bestMatch.identifier === "twenty-dollar-bill") {
+        classification = "20 DOLLAR BILL";
+      }
+      else if (this.state.bestMatch && this.state.bestMatch.identifier && this.state.bestMatch.identifier === "five-dollar-bill") {
+        classification = "FIVE DOLLAR BILL";
+      }
+      else if (this.state.bestMatch && this.state.bestMatch.identifier && this.state.bestMatch.identifier === "one-dollar-bill") {
+        classification = "ONE DOLLAR BILL";
+      }
+      else {
+        classification = "NOT DOLLAR BILL";
       }
 
     }
 
     return (
       <View style={styles.container}>
-        <CoreMLImage modelFile="HotDogOrNotHotDog" onClassification={(evt) => this.onClassification(evt)}>
+        <CoreMLImage modelFile="DollarBillModel" onClassification={(evt) => this.onClassification(evt)}>
           <View style={styles.container}>
             <Text style={styles.info}>{classification}</Text>
+            {/*{*/}
+              {/*classifications.map((classification) => {*/}
+                {/*return (*/}
+                  {/*<Text style={styles.info}>Identifier: {classification.identifier}, Confidence: {classification.confidence}</Text>*/}
+                {/*)*/}
+              {/*})*/}
+            {/*}*/}
           </View>
         </CoreMLImage>
       </View>
@@ -82,7 +96,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   info: {
-    fontSize: 20,
+    fontSize: 16,
     color: "#ffffff",
     textAlign: 'center',
     fontWeight: "900",
